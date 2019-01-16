f50c7222124a544ab965d24690b4f8896d8d297b
charlesvaez <839019311@qq.com>
2019-01-16 17:26:49 +0800

    整合spring boot2.0.4和dubbo2.x

---
+++ springboot-dubbo-api/src/main/java/com/hgsoft/springbootdubboapi/service/DemoService.java
@@ -0,0 +1,13 @@
+package com.hgsoft.springbootdubboapi.service;
+
+/**
+ * DemoService
+ * 服务Api接口类
+ * @author hcx
+ * @date 2019/1/16
+ */
+public interface DemoService {
+
+    String sayHello(String name);
+
+}
---
+++ springboot-dubbo-producer/src/main/java/com/hgsoft/springbootdubboproducer/service/impl/DemoServiceImpl.java
@@ -0,0 +1,15 @@
+package com.hgsoft.springbootdubboproducer.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.hgsoft.springbootdubboapi.service.DemoService;
+
+/**
+ *
+ */
+@Service(version = "${demo.service.version}")
+public class DemoServiceImpl implements DemoService {
+    @Override
+    public String sayHello(String name) {
+        return "Hello, " + name + " (from Spring Boot)";
+    }
+}
